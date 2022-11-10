1698b1505c9ae6c777fb5926ea3594e03fcadf03
Anastacia Protasenya <anastacia.sinitsa@gmail.com>
2022-11-10 18:11:33 +0300

    changes container

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { Container } from "./ContainerStyled";
+import { Container } from "./containerStyled";
 import { Global } from "./GlobalStyles";
 
 const root = ReactDOM.createRoot(
